6fd9072da7c215527cc86b42fbb5d76dc9ba7fc6
prostats99 <ericyougam27@gmail.com>
2025-12-19 03:44:34 -0300

    Update data.js

--- data.js
+++ data.js
@@ -4,23 +4,85 @@ const data = {
       nombre: "VillaCanto FC",
       escudo: "VillaCanto_FC.png",
       jugadores: [
-        { nombre: "Eric Gamarra", posicion: ["DEL", "EXT", "MCO"], goles: 15, asistencias: 16, pases: 312, partidos: 5, recuperaciones: 10, atajadas: 0, vallasInvictas: 0, foto: "Jugador_Generico.png" },
-        { nombre: "Daylan Gaitan", posicion: ["DEL"], goles: 17, asistencias: 4, pases: 98, partidos: 5, recuperaciones: 5, atajadas: 0, vallasInvictas: 0, foto: "Jugador_Generico.png" },
-        { nombre: "Fabricio Zapata", posicion: ["MC", "EXT"], goles: 5, asistencias: 12, pases: 110, partidos: 5, recuperaciones: 8, atajadas: 0, vallasInvictas: 0, foto: "Jugador_Generico.png" },
-        { nombre: "Alan Laurito", posicion: ["MC"], goles: 4, asistencias: 7, pases: 92, partidos: 5, recuperaciones: 12, atajadas: 0, vallasInvictas: 0, foto: "Jugador_Generico.png" },
-        { nombre: "Felipe Zapata", posicion: ["DEF"], goles: 2, asistencias: 2, pases: 164, partidos: 5, recuperaciones: 45, atajadas: 0, vallasInvictas: 0, foto: "Jugador_Generico.png" },
-        { nombre: "Isaac Sanchez", posicion: ["DEF"], goles: 3, asistencias: 3, pases: 83, partidos: 5, recuperaciones: 38, atajadas: 0, vallasInvictas: 0, foto: "Jugador_Generico.png" },
-        { nombre: "Agustín Girsa", posicion: ["POR"], goles: 0, asistencias: 0, pases: 40, partidos: 5, recuperaciones: 2, atajadas: 38, vallasInvictas: 1, foto: "Jugador_Generico.png" }
+        { 
+          nombre: "Eric Gamarra", 
+          posicion: ["DEL", "EXT", "MCO"], 
+          foto: "Jugador_Generico.png",
+          golesTotales: 7, 
+          asistenciasTotales: 8,
+          vallasInvictasTotales: 0,
+          semana: { goles: 7, asistencias: 8, vallaInvicta: false, jugo: true },
+          stats: { velocidad: 90, aceleracion: 80, tiro: 89, tecnica: 81, regates: 76, potencia: 93, resistencia: 77, defensa: 66 }
+        },
+        { 
+          nombre: "Daylan Gaitan", 
+          posicion: ["DEL"], 
+          foto: "Jugador_Generico.png",
+          golesTotales: 5, 
+          asistenciasTotales: 4,
+          vallasInvictasTotales: 0,
+          semana: { goles: 5, asistencias: 4, vallaInvicta: false, jugo: true },
+          stats: { velocidad: 91, aceleracion: 79, tiro: 85, tecnica: 77, regates: 76, potencia: 84, resistencia: 80, defensa: 77 }
+        },
+        { 
+          nombre: "Fabricio Zapata", 
+          posicion: ["MC", "EXT"], 
+          foto: "Jugador_Generico.png",
+          golesTotales: 3, 
+          asistenciasTotales: 4,
+          vallasInvictasTotales: 0,
+          semana: { goles: 3, asistencias: 4, vallaInvicta: false, jugo: true },
+          stats: { velocidad: 76, aceleracion: 80, tiro: 75, tecnica: 82, regates: 91, potencia: 72, resistencia: 74, defensa: 66 }
+        },
+        { 
+          nombre: "Alan Laurito", 
+          posicion: ["MC"], 
+          foto: "Jugador_Generico.png",
+          golesTotales: 2, 
+          asistenciasTotales: 2,
+          vallasInvictasTotales: 0,
+          semana: { goles: 2, asistencias: 1, vallaInvicta: false, jugo: false },
+          stats: { velocidad: 72, aceleracion: 76, tiro: 74, tecnica: 84, regates: 92, potencia: 70, resistencia: 76, defensa: 78 }
+        },
+        { 
+          nombre: "Felipe Zapata", 
+          posicion: ["DEF"], 
+          foto: "Jugador_Generico.png",
+          golesTotales: 1, 
+          asistenciasTotales: 1,
+          vallasInvictasTotales: 0,
+          semana: { goles: 1, asistencias: 1, vallaInvicta: false, jugo: true },
+          stats: { velocidad: 76, aceleracion: 75, tiro: 73, tecnica: 76, regates: 76, potencia: 69, resistencia: 72, defensa: 85 }
+        },
+        { 
+          nombre: "Isaac Peralta", 
+          posicion: ["DEF"], 
+          foto: "Jugador_Generico.png",
+          golesTotales: 2, 
+          asistenciasTotales: 1,
+          vallasInvictasTotales: 0,
+          semana: { goles: 2, asistencias: 1, vallaInvicta: false, jugo: false },
+          stats: { velocidad: 76, aceleracion: 75, tiro: 71, tecnica: 75, regates: 75, potencia: 77, resistencia: 78, defensa: 86 }
+        },
+        { 
+          nombre: "Agustín Girsa", 
+          posicion: ["POR"], 
+          foto: "Jugador_Generico.png",
+          golesTotales: 0, 
+          asistenciasTotales: 0,
+          vallasInvictasTotales: 10,
+          semana: { goles: 0, asistencias: 0, vallaInvicta: true, jugo: true },
+          stats: { velocidad: 90, aceleracion: 78, tiro: 72, tecnica: 88, regates: 76, potencia: 89, resistencia: 80, defensa: 87 }
+        }
       ]
     },
     {
-      nombre: "SC VillaRiccio",
-      escudo: "equipo_generico.jpg",
-      jugadores: [] 
+      nombre: "SC VillaCampi",
+      escudo: "SC_VillaCampi.png",
+      jugadores: [] //por ahora no hay, los agrego despues 
     }
   ],
   partidos: [
-    { local: "VillaCanto FC", visitante: "SC VillaRiccio", scoreL: 3, scoreV: 2, fecha: "15 de Junio" },
-    { local: "SC VillaRiccio", visitante: "VillaCanto FC", scoreL: 0, scoreV: 5, fecha: "20 de Junio" }
+    { local: "VillaCanto FC", visitante: "SC VillaRiccio", scoreL: 20, scoreV: 12, fecha: "15 de Junio" },
   ]
 };
