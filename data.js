209be244e3f1994a8b3f505055271a7dd3deca90
prostats99 <ericyougam27@gmail.com>
2025-12-22 20:56:56 -0300

    Update data.js

--- data.js
+++ data.js
@@ -6,38 +6,46 @@ const data = {
       jugadores: [
         { 
           nombre: "Eric Gamarra", 
-          posicion: ["DEL", "EXT", "MCO"], 
+          posicion: [ "DEL","EXT", "MCO"], 
           foto: "Jugador_Generico.png",
-          golesTotales: 7, 
+          piernaHabil: "Derecha",
+          piernaMala: "4",
+          golesTotales: 10, 
           asistenciasTotales: 8,
           vallasInvictasTotales: 0,
-          semana: { goles: 7, asistencias: 8, vallaInvicta: false, jugo: true },
+          semana: { goles:7, asistencias: 8, vallaInvicta: false, jugo: true },
           stats: { velocidad: 90, aceleracion: 80, tiro: 89, tecnica: 81, regates: 76, potencia: 93, resistencia: 77, defensa: 66 }
         },
         { 
           nombre: "Daylan Gaitan", 
           posicion: ["DEL"], 
           foto: "Jugador_Generico.png",
+          piernaHabil: "Derecha",
+          piernaMala: "2",
           golesTotales: 5, 
           asistenciasTotales: 4,
           vallasInvictasTotales: 0,
           semana: { goles: 5, asistencias: 4, vallaInvicta: false, jugo: true },
-          stats: { velocidad: 91, aceleracion: 79, tiro: 85, tecnica: 77, regates: 76, potencia: 84, resistencia: 80, defensa: 77 }
+          stats: { velocidad: 91, aceleracion: 79, tiro: 86, tecnica: 77, regates: 76, potencia: 85, resistencia: 80, defensa: 77 }
         },
         { 
           nombre: "Fabricio Zapata", 
           posicion: ["MC", "EXT"], 
           foto: "Jugador_Generico.png",
+          piernaHabil: "Derecha",
+          piernaMala: "3",
           golesTotales: 3, 
           asistenciasTotales: 4,
           vallasInvictasTotales: 0,
           semana: { goles: 3, asistencias: 4, vallaInvicta: false, jugo: true },
-          stats: { velocidad: 76, aceleracion: 80, tiro: 75, tecnica: 82, regates: 91, potencia: 72, resistencia: 74, defensa: 66 }
+          stats: { velocidad: 76, aceleracion: 80, tiro: 75, tecnica: 84, regates: 91, potencia: 72, resistencia: 74, defensa: 66 }
         },
         { 
           nombre: "Alan Laurito", 
           posicion: ["MC"], 
           foto: "Jugador_Generico.png",
+          piernaHabil: "Derecha",
+          piernaMala: "2",
           golesTotales: 2, 
           asistenciasTotales: 2,
           vallasInvictasTotales: 0,
@@ -48,6 +56,8 @@ const data = {
           nombre: "Felipe Zapata", 
           posicion: ["DEF"], 
           foto: "Jugador_Generico.png",
+          piernaHabil: "Derecha",
+          piernaMala: "2",
           golesTotales: 1, 
           asistenciasTotales: 1,
           vallasInvictasTotales: 0,
@@ -58,6 +68,8 @@ const data = {
           nombre: "Isaac Peralta", 
           posicion: ["DEF"], 
           foto: "Jugador_Generico.png",
+          piernaHabil: "Derecha",
+          piernaMala: "2",
           golesTotales: 2, 
           asistenciasTotales: 1,
           vallasInvictasTotales: 0,
@@ -68,18 +80,54 @@ const data = {
           nombre: "Agustín Girsa", 
           posicion: ["POR"], 
           foto: "Jugador_Generico.png",
+          piernaHabil: "Derecha",
+          piernaMala: "2",
           golesTotales: 0, 
           asistenciasTotales: 0,
           vallasInvictasTotales: 10,
           semana: { goles: 0, asistencias: 0, vallaInvicta: true, jugo: true },
           stats: { velocidad: 90, aceleracion: 78, tiro: 72, tecnica: 88, regates: 76, potencia: 89, resistencia: 80, defensa: 87 }
-        }
-      ]
+        },
+        { 
+          nombre: "Facundo Marcatelli", 
+          posicion: ["DEF"], 
+          foto: "Jugador_Generico.png",
+          piernaHabil: "Derecha",
+          piernaMala: "2",
+          golesTotales: 3, 
+          asistenciasTotales: 0,
+          vallasInvictasTotales: 0,
+          semana: { goles: 3, asistencias: 0, vallaInvicta: false, jugo: true },
+          stats: { velocidad: 70, aceleracion: 69, tiro: 78, tecnica: 73, regates: 67, potencia: 73, resistencia: 74, defensa: 81 }
+        },
+        { 
+          nombre: "Luciano Gonzalez", 
+          posicion: ["MC"], 
+          foto: "Jugador_Generico.png",
+          piernaHabil: "Derecha",
+          piernaMala: "2",
+          golesTotales: 1, 
+          asistenciasTotales: 2,
+          vallasInvictasTotales: 0,
+          semana: { goles: 0, asistencias: 0, vallaInvicta: false, jugo: true },
+          stats: { velocidad: 78, aceleracion: 78, tiro: 76, tecnica: 78, regates: 80, potencia: 72, resistencia: 74, defensa: 66 }
+        }      ]
     },
     {
       nombre: "SC VillaCampi",
       escudo: "SC_VillaCampi.png",
-      jugadores: [] //por ahora no hay, los agrego despues 
+      jugadores: [
+        { 
+          nombre: "Emiliano Garcilazo", 
+          posicion: ["DEF"], 
+          foto: "Jugador_Generico.png",
+          golesTotales: 3, 
+          asistenciasTotales: 0,
+          vallasInvictasTotales: 0,
+          semana: { goles: 3, asistencias: 0, vallaInvicta: false, jugo: true },
+          stats: { velocidad: 69, aceleracion: 68, tiro: 81, tecnica: 75, regates: 68, potencia: 96, resistencia: 77, defensa: 80 }
+        }
+      ] //
     }
   ],
   partidos: [
